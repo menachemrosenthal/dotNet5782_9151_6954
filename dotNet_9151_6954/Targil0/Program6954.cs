0c6f3aa78bfdf00acba3edc787080a213a540d3b
itzickwalles <walles@g.jct.ac.il>
2021-10-10 22:31:01 +0300

    Initial code + Welcome functions

---
+++ dotNet_9151_6954/Targil0/Program6954.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6954();
+            Welcome9151();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome9151();
+        private static void Welcome6954()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+    }
+}
