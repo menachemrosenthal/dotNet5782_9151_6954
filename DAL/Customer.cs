9a4814601371ab7f1679fb38c13f63f67d33581c
menachemtr96@gmail.com <menachemtr96@gmail.com>
2021-10-21 22:27:52 +0300

    these are dal classes

---
+++ DAL/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+
+
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+
+        }
+    }
+}
+
+
