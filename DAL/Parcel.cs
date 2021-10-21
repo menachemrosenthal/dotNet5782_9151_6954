96d36c2168a0f1bf4ef676025815cd45d0624564
Itzik <walles@jct.ac.il>
2021-10-21 22:26:51 +0300

    create classes

---
+++ DAL/Parcel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DP
+    {
+        public struct Parcel
+        {
+            public int Id { get; set; }
+            public int Senderid { get; set; }
+            public int TargetId { get; set; }
+            public string Weight { get; set; }
+            public string Proirity { get; set; }
+            public int DroindId { get; set; }
+            public string Requested { get; set; }
+            public string Scheduled { get; set; }
+            public string PickedUp { get; set; }
+            public string Delivered { get; set; }
+        }
+    }
+}
