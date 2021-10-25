f2861325ceabd37782bc1f58842838b94f50b146
Itzik <walles@jct.ac.il>
2021-10-25 11:09:44 +0300

    creat consoleUI and DAL

---
+++ DAL/Drone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public double Battery { get; set; }
+            public string MaxWeight { get; set; }
+            public string Status { get; set; }
+
+        }
+    }
+}
+
