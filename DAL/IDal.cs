daa3b622dcb30cc6c3bec8275c1622016968c7e5
Itzik <walles@jct.ac.il>
2021-11-14 22:52:04 +0200

    create idal interface

---
+++ DAL/IDal.cs
@@ -0,0 +1,15 @@
+﻿using IDAL.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    interface IDal
+    {
+        void AddStation(Station station);
+
+    }
+}
