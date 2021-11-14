011b76a1c9d42ac37daa1068476a12f81a550ec5
menachemtr96@gmail.com <menachemtr96@gmail.com>
2021-11-14 22:59:42 +0200

    IDAL functions

--- DAL/IDal.cs
+++ DAL/IDal.cs
@@ -10,6 +10,14 @@ namespace IDAL
     interface IDal
     {
         void AddStation(Station station);
+        Drone GetDrone(int droneId);
+        Customer GetCustomer(int customerId);
+        Parcel GetParcel(int parcelId);
+        IEnumerable<Station> StationList();
+        IEnumerable<Customer> CustomerList();
+        IEnumerable<Parcel> ParcelList();
+        IEnumerable<Drone> DroneList();
+        double DistanceCalculate(double lat1, double lon1, double lat2, double lon2);
 
     }
 }
