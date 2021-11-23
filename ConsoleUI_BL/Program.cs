8c8c17b487323c8c9f09daebf3119f214a6c4c34
menachemtr96@gmail.com <menachemtr96@gmail.com>
2021-11-23 13:54:24 +0200

    location range

--- ConsoleUI_BL/Program.cs
+++ ConsoleUI_BL/Program.cs
@@ -368,16 +368,16 @@ namespace ConsoleUI_BL
             station.Name = Console.ReadLine();
             Console.WriteLine("\nENTER Charge slots");
             _ = int.TryParse(Console.ReadLine(), out int chargeSlots);
-            Console.WriteLine("\nENTER longitude");
+            Console.WriteLine("\nENTER longitude  from 34.955762 to 34.959020");
             _ = double.TryParse(Console.ReadLine(), out double longitude);
-            Console.WriteLine("\nENTER latitude");
-            _ = double.TryParse(Console.ReadLine(), out double latitude);
+            Console.WriteLine("\nENTER latitude from 31.589844 to  32.801705");
+            _ = double.TryParse(Console.ReadLine(), out double lattitude);
 
             station.Id = id; station.ChargeSlots = chargeSlots;
             station.LocationOfStation = new()
             {
                 Longitude = longitude,
-                Latitude = latitude
+                Latitude = lattitude
             };
             bl.AddStation(station);
         }
@@ -409,9 +409,9 @@ namespace ConsoleUI_BL
             customer.Name = Console.ReadLine();
             Console.WriteLine("\nENTER phone");
             customer.Phone = Console.ReadLine();
-            Console.WriteLine("\nENTER longitude");
+            Console.WriteLine("\nENTER longitude  from 34.955762 to 34.959020");
             _ = double.TryParse(Console.ReadLine(), out double longitude);
-            Console.WriteLine("\nENTER latitude");
+            Console.WriteLine("\nENTER latitude from 31.589844 to  32.801705");
             _ = double.TryParse(Console.ReadLine(), out double lattitude);
 
             customer.Id = id; customer.Location.Longitude = longitude;
